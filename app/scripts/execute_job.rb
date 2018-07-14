1c1daa66adc14a0616414abddee9850a38e51321
Anca Sarbu <asarbu@everfi.com>
2018-07-14 17:58:58 +0300

    edit execute_job

--- app/scripts/execute_job.rb
+++ app/scripts/execute_job.rb
@@ -3,7 +3,10 @@ gym_class_id = ARGV[1]
 
 return if user_id.nil? || gym_class_id.nil?
 
-user = User.find user_id
-gym_class = GymClass.find gym_class_id
+user = User.find_by id: user_id
+gym_class = GymClass.find_by id: gym_class_id
+
+
+return if user.nil? || gym_class.nil?
 
 system("sh app/scripts/gym_scheduler.sh #{user.email} #{user.decoded_password } #{gym_class.platform_id}")
