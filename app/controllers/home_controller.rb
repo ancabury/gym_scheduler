5c19069c8043f38b0b7fee025592b4d152555324
Anca Sarbu <anca.sarbu.3pg@cofense.com>
2018-09-05 13:22:21 +0300

    Update index order for gym classes. They will be order by :start_at as well

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @gym_classes = GymClass.all.order(day_of_week: :asc)
+    @gym_classes = GymClass.all.order(day_of_week: :asc, start_at: :asc)
   end
 end
