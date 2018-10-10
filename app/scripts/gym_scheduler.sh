d8fdb99c879fbd70d4eae5fffcc68bad796e185b
Anca Sarbu <anca.sarbu.3pg@cofense.com>
2018-10-10 14:54:50 +0300

    add some prints to bash script

--- app/scripts/gym_scheduler.sh
+++ app/scripts/gym_scheduler.sh
@@ -12,7 +12,10 @@ portal_login_url=$(echo $login_page | sed -e 's/.*<a href="\(.*\)">.*/\1/')
 # Login into the application
 login_response=$(curl -XPOST https://cbcfitness.gymmasteronline.com$portal_login_url \
                       -d "email="$email"&password="$pass\
-                      -H "Content-Type: application/x-www-form-urlencoded")
+                      -H 'Content-Type: application/x-www-form-urlencoded')
+
+echo "================= Login response ================="
+echo $login_response
 
 # get the session for the logged user
 account_session=$(echo $login_response | sed -e 's/.*<a href="\/portal\/account\/?session=\(.*\)">.*/\1/')
@@ -20,11 +23,17 @@ account_session=$(echo $login_response | sed -e 's/.*<a href="\/portal\/account\
 # Submit a class booking
 response_location=$(curl -i -XPOST "https://cbcfitness.gymmasteronline.com/portal/account/bookclass/submit?s=account.bookclass&session="$account_session\
                          -d 'bookingrequests=[{"bookingparentid":'$class_id'}]'\
-                         -H "Content-Type: application/x-www-form-urlencoded"\
+                         -H 'Content-Type: application/x-www-form-urlencoded'\
                          | grep "Location")
+
+echo "================= Response location ================="
+echo $response_location
 
 # get booking id
 booking_id=$(echo $response_location | sed -e 's/.*\/portal\/payment\/tacs\/\(.*\)?session.*/\1/')
+
+echo "================= Booking id ================="
+echo $booking_id
 
 # booking confirmation
 curl -XPOST https://cbcfitness.gymmasteronline.com/portal/payment/complete/$booking_id?session=$account_session
